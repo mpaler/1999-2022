d312195fbbe8c8f1bc61043e494d9ee9b8ecb92a
swaylock <admin@swaylocks.com>
2023-01-28 17:50:16 -0800

    Add in sponsors section to homepage.

---
+++ modules/swaylocks_base/theme/googlesearch.tpl.php
@@ -0,0 +1,12 @@
+<?php ?>
+<div class="row m-0 mb-4 pb-1 pt-1" style="background-color: lightgray; border-radius: 5px;">
+  <div class="col col-12 pl-1 pr-1">
+    <div class="google_search">
+      <section id="google_search" class="section">
+        <script async src="https://cse.google.com/cse.js?cx=003250312232961279926:i8tfrcq3eyy">
+        </script>
+        <div class="gcse-search"></div>
+      </section>
+    </div>
+  </div>
+</div>
